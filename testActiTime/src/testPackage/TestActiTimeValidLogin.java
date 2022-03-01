a6be68a7ef1ca826628fbfbe5d68b94812457ac6
HP <HP@DESKTOP-IHA6RKJ>
2022-03-01 09:20:50 +0530

    My first framework

---
+++ testActiTime/src/testPackage/TestActiTimeValidLogin.java
@@ -0,0 +1,24 @@
+package testPackage;
+
+import java.io.IOException;
+
+import generic.BaseTest;
+import generic.Flib;
+import pagePackage.LoginPage;
+
+public class TestActiTimeValidLogin  extends BaseTest{
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		BaseTest bt = new BaseTest();
+		bt.openBrowser();//open the browser and launch the web app
+		LoginPage lp = new LoginPage(driver);
+		
+		Flib flib = new Flib();
+		String validUsername = flib.readPropertyData(PROP_PATH, "un");
+		String validPassword = flib.readPropertyData(PROP_PATH, "pass");
+		
+		lp.validLogin(validUsername, validPassword);//valid login
+		bt.closeBrowser();
+	}
+
+}
