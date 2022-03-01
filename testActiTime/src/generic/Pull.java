1549f4d7327a3b82dff818c88a2569b69d93b4ed
HP <HP@DESKTOP-IHA6RKJ>
2022-03-01 12:25:56 +0530

    pull

---
+++ testActiTime/src/generic/Pull.java
@@ -0,0 +1,11 @@
+package generic;
+
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
+public class Pull {
+
+	public static void main(String[] args) {
+		System.out.println("Nita");
+	}
+
+}
